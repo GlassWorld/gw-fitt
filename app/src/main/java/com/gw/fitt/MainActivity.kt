3d35892375a9b83947cb3a3a62bd521d2aae1977
GlassWorld <chjsa12@gmail.com>
2026-05-02 21:26:27 +0900

    기능: 다크 테마와 시작 화면 적용

--- app/src/main/java/com/gw/fitt/MainActivity.kt
+++ app/src/main/java/com/gw/fitt/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
@@ -34,6 +35,8 @@ private fun FittApp() {
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.onBackground,
         bottomBar = { FittBottomNav(navController = navController) }
     ) { innerPadding ->
         FittNavGraph(
